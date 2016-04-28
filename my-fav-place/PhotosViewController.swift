8ddf853c751bcc026518b1403763dcfd597f6fa1
Amadeu Andrade <amadeuandrade@Amadeus-MBP.home>
2016-04-28 23:21:44 +0100

    Added images and swipe to left option

--- my-fav-place/PhotosViewController.swift
+++ my-fav-place/PhotosViewController.swift
@@ -10,26 +10,46 @@ import UIKit
 
 class PhotosViewController: UIViewController {
 
+    var photosArray = ["night", "barcos", "ribeira", "bolhao", "caves"]
+    var imageIndex = 0
+
+    @IBOutlet weak var photoImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(PhotosViewController.swiped(_:))) // put : at the end of method name
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(PhotosViewController.swiped(_:))) // put : at the end of method name
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        photoImg.image = UIImage(named:"night")
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func swiped(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.Right : break
+                
+                /*No clue how to make it go back to the previous image and
+                 when it hits the last image in the array, it goes back to
+                 the first image.. */
+                
+            case UISwipeGestureRecognizerDirection.Left:
+                if imageIndex >= photosArray.count {
+                    imageIndex = 0
+                }
+                photoImg.image = UIImage(named: photosArray[imageIndex])
+                imageIndex += 1
+                
+            default:
+                break //stops the code/codes nothing.
+            }
+        }
     }
-    */
 
 }
