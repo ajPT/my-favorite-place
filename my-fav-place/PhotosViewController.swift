2a68491b5702a9ef714402c7fd520801373a57e5
Amadeu Andrade <amadeuandrade@Amadeus-MBP.home>
2016-04-28 23:33:14 +0100

    Finished the swipe definition

--- my-fav-place/PhotosViewController.swift
+++ my-fav-place/PhotosViewController.swift
@@ -18,36 +18,36 @@ class PhotosViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(PhotosViewController.swiped(_:))) // put : at the end of method name
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(PhotosViewController.swiped(_:)))
         swipeRight.direction = UISwipeGestureRecognizerDirection.Right
         self.view.addGestureRecognizer(swipeRight)
         
-        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(PhotosViewController.swiped(_:))) // put : at the end of method name
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(PhotosViewController.swiped(_:)))
         swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
         self.view.addGestureRecognizer(swipeLeft)
         
-        photoImg.image = UIImage(named:"night")
-        
+        photoImg.image = UIImage(named:photosArray[imageIndex])
     }
     
     func swiped(gesture: UIGestureRecognizer) {
         if let swipeGesture = gesture as? UISwipeGestureRecognizer {
             switch swipeGesture.direction {
-            case UISwipeGestureRecognizerDirection.Right : break
+                case UISwipeGestureRecognizerDirection.Right:
+                    imageIndex -= 1
+                    if imageIndex < 0 {
+                        imageIndex += photosArray.count
+                    }
+                    photoImg.image = UIImage(named: photosArray[imageIndex])
                 
-                /*No clue how to make it go back to the previous image and
-                 when it hits the last image in the array, it goes back to
-                 the first image.. */
+                case UISwipeGestureRecognizerDirection.Left:
+                    if imageIndex >= photosArray.count {
+                        imageIndex = 0
+                    }
+                    photoImg.image = UIImage(named: photosArray[imageIndex])
+                    imageIndex += 1
                 
-            case UISwipeGestureRecognizerDirection.Left:
-                if imageIndex >= photosArray.count {
-                    imageIndex = 0
-                }
-                photoImg.image = UIImage(named: photosArray[imageIndex])
-                imageIndex += 1
-                
-            default:
-                break //stops the code/codes nothing.
+                default:
+                    break
             }
         }
     }
